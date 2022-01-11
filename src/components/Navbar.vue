5306e7ab8e969731fb9e82f91b8fceecb3ed72df
Jaideep <78426970+jaideep-git@users.noreply.github.com>
2022-01-11 16:39:35 -0500

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,8 +8,7 @@
 				<router-link to="/Movies" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" data-v-7b357a42=""><g fill="none" stroke="#fff" stroke-width="1.5" stroke-miterlimit="10" stroke-linejoin="round" stroke-linecap="round" data-v-7b357a42=""><path d="M3.2 12.8h19.6v9.5c0 .5-.4.9-1 .9H4.1c-.5 0-1-.4-1-.9v-9.5" data-v-7b357a42=""></path><path d="M3.3 13.1l-2-4.4c-.2-.5 0-1 .5-1.2L18 .8c.5-.2 1.1 0 1.3.5l1.8 4-17.5 7.3" data-v-7b357a42=""></path><path d="M15 2.1l-.9 6M8 4.7l-1.2 6.6" data-v-7b357a42=""></path></g></svg></router-link>
 			</li>
             <li id="tv">
-                <router-link to="/Shows" >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" data-v-7b357a42=""><g fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-miterlimit="10" data-v-7b357a42=""><path d="M21.4 23H2.6c-.9 0-1.6-.7-1.6-1.6V8.9c0-.9.7-1.6 1.6-1.6h18.9c.8 0 1.5.7 1.5 1.6v12.6c0 .8-.7 1.5-1.6 1.5zM6.4 1L12 7M17.6 1L12 7" data-v-7b357a42=""></path></g></svg></router-link>
+                <router-link to="/Shows" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" data-v-7b357a42=""><g fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-miterlimit="10" data-v-7b357a42=""><path d="M21.4 23H2.6c-.9 0-1.6-.7-1.6-1.6V8.9c0-.9.7-1.6 1.6-1.6h18.9c.8 0 1.5.7 1.5 1.6v12.6c0 .8-.7 1.5-1.6 1.5zM6.4 1L12 7M17.6 1L12 7" data-v-7b357a42=""></path></g></svg></router-link>
             </li>
         </ul>
         <form>
@@ -35,6 +34,7 @@ export default {
         async getSearch(){
             const getSearchItems = await axios.get(`https://api.themoviedb.org/3/search/multi?api_key=37ed43a4f8eaa2abd75f9283692947bc&language=en-US&page=1&query=${this.searchInput}`)
             this.$parent.searchResults=getSearchItems.data.results
+            this.searchInput=""
             console.log(this.$parent.searchResults)
         },
         clear(){
